449599f9d8c104609302403ca724f5c1813094de
Shaun <shjsawyer@gmail.com>
2018-06-09 13:32:47 -0400

    subsetsum bottom up

--- dp/subsetsum.py
+++ dp/subsetsum.py
@@ -1,4 +1,4 @@
-def subsetsum(setofnums, value):
+def subsetsum(setofnums, value, method='recursive'):
     '''
     Given the set of nonnegative integers `setofnums` and the integer `value`,
     determine if there is a subset of element in `setofnums` that sum to `value`.
@@ -28,11 +28,47 @@ def subsetsum(setofnums, value):
             # add ourselves to sum to the value `v`
             return _subsetsum(n-1, v) or _subsetsum(n-1, v-setofnums[n])
 
-    return _subsetsum(len(setofnums)-1, value)
+    def _subsetsum_bu(setofnums, value):
+        '''
+        Solves the problem working bottom up by starting with the base cases
+        and iteratively extending optimal solutions for smaller values of `n`
+        to larger values of `n`.
+        '''
+
+        # E[n][v] is True if there is a subset of `setofnums` up in index
+        # `n` that adds to the value `v`, and False otherwise
+        E = [[None for v in xrange(value+1)] for n in range(len(setofnums))]
+
+        # We are recursing on n, so we need n = 0 populated
+        # n = 0 corresponds to when we only have the first item, so E[n][v]
+        # will be true if v == 0 or v == setofnums[n]
+        for v in range(value+1):
+            E[0][v] = (v == 0) or (v == setofnums[0])
+        # We are also recursing on v, so we need v = 0 populated
+        # E[n][0] = True for all n (can always take 0 items to have value of 0)
+        for n in range(len(setofnums)):
+            E[n][0] = True
+
+        # Populate the rest of the table
+        for n in range(1, len(setofnums)):
+            for v in range(1, value+1):
+                if v - setofnums[n] < 0:
+                    E[n][v] = E[n-1][v]
+                else:
+                    E[n][v] = E[n-1][v] or E[n-1][v - setofnums[n]]
+
+        # return the value we are interested in
+        return E[len(setofnums)-1][value]
+
+    # Use the specified method
+    if method == 'recursive':
+        return _subsetsum(len(setofnums)-1, value)
+    elif method == 'bottomup':
+        return _subsetsum_bu(setofnums, value)
 
 
 def main():
-    print subsetsum([3, 9, 2, 1, 5], 15)
+    print subsetsum([3, 9, 2, 1, 5], 100, method='bottomup')
 
 
 if __name__ == '__main__':
