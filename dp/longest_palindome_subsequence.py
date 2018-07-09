d267d828a72db805b07e49a0070705b68a8c7cfb
Shaun <shjsawyer@gmail.com>
2018-07-09 19:57:13 -0400

    lps, dp to get length with recursion for actual sequence

--- dp/longest_palindome_subsequence.py
+++ dp/longest_palindome_subsequence.py
@@ -13,6 +13,10 @@ A: Given an input `s` of length n, we can use the following recursive relation
            max(lps(s[i+1..j]), lps(s[i..j-1])) otherwise
            }
 
+   Note that the recursive relation can involve increasing one index while
+   decreasing another. This leads to an interesting way in which we must
+   build our dp table.
+
 '''
 
 def longest_palindrome_subsequence(s):
@@ -30,31 +34,57 @@ def longest_palindrome_subsequence(s):
     # s[i..j]
     dp = [[None for _ in range(len(s))] for _ in range(len(s))]
 
-    # The length of a one character palindrome is 1
-    for i in range(len(s)):
-        dp[i][i] = 1
-
-    # Fill in the dp table bottom up
-    # NOTE:
+    # Fill in the dp table bottom up:
+    #
     # The trick here is we need to iterate over the values in the table in such
     # a way that we will have the answer to a subproblem when we need it
-    # How to do this?
-    # Because we initialize our table on the diagonal (a LPS of length 1), we
-    # must extend our solutions out from the diagonal, essentially gradually
-    # extending the length of the LPS from 1 to the length of `s`
-    for char_len in range(0, len(s)):
-        for i in range(0, len(s)):
-            j == i + char_len
-            # We now iterate out from the diagonal
+    #
+    # Notice here we require solutions to the "inner" subproblems (when we
+    # recurse, we increase the left pointer and decrease the right pointer)
+    # As such, we need to first populate the diagonal (lps of length 1), and
+    # gradually fill out the table outward from the diagonal
 
-    for i in range(1, len(s)):
-        for j in range(1, len(s)):
-            if s[i] == s[j] and i+1<len(s):
+    # Consider all valid sequence lengths
+    for char_len in range(1, len(s)+1):
+        # The left index can range between these values
+        for i in range(0, len(s)-char_len+1):
+            # The right index for this iteration
+            j = i + char_len - 1
+            if i == j:
+                # Base case: one character palindrome
+                dp[i][j] = 1
+            elif s[i] == s[j] and j == i+1:
+                # Edge case: 2 character palindrome
+                dp[i][j] = 2 if s[i] == s[j] else 1
+            elif s[i] == s[j] and j > i+1:
                 dp[i][j] = 2 + dp[i+1][j-1]
             else:
                 dp[i][j] = max(dp[i+1][j], dp[i][j-1])
 
+    # work backwards to find the lps
+    # can do this iteratively, but this recursive approach is elegant if you
+    # forgive the many list concatenations
+    def _lps_from_table(i, j):
+        if i == j:
+            return list(s[i])
+        elif j == i+1 and s[i] == s[j]:
+            return [s[i], s[j]]
+        elif j > i+1 and s[i] == s[j]:
+            return list(s[i]) + _lps_from_table(i+1, j-1) + list(s[j])
+        else:
+            i, j = (i+1, j) if dp[i+1][j] > dp[i][j-1] else (i, j-1)
+            return _lps_from_table(i, j)
+
+    lps = _lps_from_table(0, len(s)-1)
+    # return a string
+    return "".join(lps)
 
 
+def main():
+    seqs = 'adbbebebca', "hello", "a", "12321", "byebye"
+    for seq in seqs:
+        print "lps({}): {}".format(seq, longest_palindrome_subsequence(seq))
 
 
+if __name__ == '__main__':
+    main()
