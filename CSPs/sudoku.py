eca8fdf774cd8a5c6e5d67ce31f2cfa8272ea250
Shaun <shjsawyer@gmail.com>
2018-07-18 19:45:06 -0400

    logic for most constraining variable and least constraining value

--- CSPs/sudoku.py
+++ CSPs/sudoku.py
@@ -1,11 +1,42 @@
 import math
 
 
-def order_domain_values(square):
+def constrained(square1, square2, board):
+    '''
+    Determine if two squares are part of a binary constraint
+    E.g., in the same row, column or subgrid of the sudoku board
+    '''
+    if square1 == square2:
+        return False
+    if not constrained.hasattr("subgridsize"):
+        constrained.subgridsize = math.sqrt(len(board))
+    if ((square1[0] == square2[0])    # same row
+      or (square1[1] == square2[1])   # same column
+      or ((square1[0]/constrained.subgridsize ==
+           square2[0]/constrained.subgridsize) and
+          (square1[1]/constrained.subgridsize ==
+           square2[1]/constrained.subgridsize))):  # same subgrid
+        return True
+    # Not constrained
+    return False
+
+
+def order_domain_values(board, domains, square):
     '''
     Use the Least Constraining Values heuristic to order the values of `square`
+    given the current state of `board` and `domains`
     '''
-    pass
+    n_constraints = {value: 0 for value in domains[square[0]][square[1]]}
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if not assigned((i, j), board) and\
+               constrained(square, (i, j)):
+                for value in n_constraints:
+                    if value in domains[i][j]:
+                        n_constraints[value] += 1
+    value_orders = sorted(n_constraints.keys(),
+                          key=lambda v: n_constraints[v])
+    return value_orders
 
 
 def select_free_square(board, domains):
@@ -27,18 +58,38 @@ def select_free_square(board, domains):
                     mcvs = [(i, j)]
                 elif len(domains[i][j]) == min_domain_size:
                     mcvs.append((i, j))
-    # Return the most constraining square amongst the most contained
-    return most_constraining_variable(mcvs)
+    # Return the most constraining square amongst the most constrained
+    return most_constraining_variable(board, mcvs)
 
 
-def most_constraining_variable(squares):
+def assigned(square, board):
+    ''' Check if `square` is currently assigned a value in `board` '''
+    return board[square[0]][square[1]] == 0
+
+
+def most_constraining_variable(board, squares):
     '''
-    Return the most constraining square amongst squares, where the domain sizes
-    of the squares in `squares` are all equal and minimum amongst all squares
-    in `board`. To do this, we return the square which is involved in the most
-    constraints.
+    Return the most constraining square amongst `squares`, where the domain
+    sizes of the squares in `squares` are all equal and minimum amongst all
+    squares in `board`. To do this, we return the square which is involved in
+    the most constraints.
     '''
-    pass
+    max_num_constraints = -1
+    mcv = None
+    # Quick check in case we don't need to do any computation
+    if len(squares) == 1:
+        return squares[0]
+    for square in squares:
+        # Find how many constraints this square is currently involved in
+        n_constraints = 0
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if (board[i][j] == 0) and constrained(square, (i, j)):
+                    n_constraints += 1
+        if n_constraints > max_num_constraints:
+            max_num_constraints = n_constraints
+            mcv = square
+    return mcv
 
 
 def AC3(board):
@@ -54,7 +105,7 @@ def assign_value(board, square, value):
     '''
     Assign the `square` in `board` to have the value `value`
     '''
-    pass
+    board[square[0]][square[1]] = value
 
 
 def add_back_pruned(board, pruned):
@@ -77,7 +128,7 @@ def recursively_backtrack(board, domains, moves_remaining):
     # Select a "good" square to assign to next
     square = select_free_square(board, domains)
     # Select the next "best" value for this square
-    for value in order_domain_values(square):
+    for value in order_domain_values(board, domains, square):
         assign_value(board, square, value)
         # Reduce the domain of other squares if possible
         pruned = AC3(board)
