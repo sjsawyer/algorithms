bb8aaf3f1f3cb943227a8d9d0728e902b3705aeb
Shaun <shjsawyer@gmail.com>
2018-08-02 10:04:58 -0400

    backtracking combinations of length k

---
+++ backtracking/combinations_of_length.py
@@ -0,0 +1,30 @@
+def combinations_of_length(arr, k):
+    '''
+    Find all combinations of elements of length k in arr
+    '''
+    all_combinations = []
+    def _combinations(sofar, remaining):
+        if len(sofar) == k:
+            # Ensure we copy the list to avoid mutation
+            all_combinations.append(sofar[:])
+            return
+        if len(remaining) == 0:
+            return
+        # all combinations using the last letter of `remaining`
+        sofar.append(remaining.pop())
+        _combinations(sofar, remaining)
+        # remove the letter
+        remaining.append(sofar.pop())
+        # all combinations without the last letter of `remaining`
+        _combinations(sofar, remaining[:-1])
+
+    _combinations([], arr)
+    return all_combinations
+
+
+def main():
+    print combinations_of_length([1, 2, 3, 4], 2)    
+
+
+if __name__ == '__main__':
+    main()
