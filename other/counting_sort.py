c064951d8029767ece0db25fcb98a4620dbcdf75
Shaun <shjsawyer@gmail.com>
2018-08-02 09:44:20 -0400

    counting sort

--- other/counting_sort.py
+++ other/counting_sort.py
@@ -7,9 +7,10 @@ element will be. In addition, it is a stable sort, meaning that if two items
 have the same key, then their relative order will be unchanged in the final
 array, making counting sort an ideal subroutine of radix sort.
 
-If there are n items in the array, the range of keys is k, then
+If there are n items in the array, the xrange of keys is k, then
 
-    Time complexity:  O(n+k) Space complexity: O(n+k)
+    Time complexity:  O(n+k)
+    Space complexity: O(n+k)
 
 '''
 
@@ -18,22 +19,21 @@ def counting_sort(a, max_element, min_element=0):
     Sort the integers in the array `a` where the minimum element in the array
     is `min_element` and the maximum element is `max_element`
     '''
-    import pdb; pdb.set_trace()
-    counts = [0 for _ in range(max_element-min_element+1)]
+    counts = [0 for _ in xrange(max_element-min_element+1)]
     # count the number of occurences of each element
-    for i in range(len(a)):
+    for i in xrange(len(a)):
         counts[a[i]-min_element] += 1
     # calculate the starting index of each element in the sorted array by
     # computing an accumulative sum of the `counts` array, and shifting
     # all elements to the right by one, setting the first element to 0
-    for i in range(1, len(counts)):
+    for i in xrange(1, len(counts)):
         counts[i] += counts[i-1]
-    for i in reversed(range(1, len(counts))):
+    for i in reversed(xrange(1, len(counts))):
         counts[i] = counts[i-1]
     counts[0] = 0
     # Create the sorted array
-    s = [None for _ in range(len(a))]
-    for i in range(len(a)):
+    s = [None for _ in xrange(len(a))]
+    for i in xrange(len(a)):
         current_element = a[i]
         index = counts[current_element-min_element]
         s[index] = current_element
@@ -45,18 +45,14 @@ def counting_sort(a, max_element, min_element=0):
 
 def main():
     import random
-    k_lower, k_upper = 0, 20
-    sample = range(k_lower, k_upper+1)
-    n = 100000
-    a = [random.choice(sample) for _ in range(n)]
-    #print a
-    import time
-    t = time.time()
-    counting_sort(a, k_upper, k_lower)
-    print "counting sort: {}".format(time.time() - t)
-    t2 = time.time()
-    sorted(a)
-    print "sorted: {}".format(time.time() - t2)
+    k_lower, k_upper = 5, 15
+    n = 20
+    sample = xrange(k_lower, k_upper+1)
+    # randomly choose `n` elements from `sample`
+    a = [random.choice(sample) for _ in xrange(n)]
+    print a
+    s = counting_sort(a, k_upper, k_lower)
+    print s
 
 
 if __name__ == '__main__':
