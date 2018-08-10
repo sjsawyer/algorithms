9a67c026e70df440af2a4ef6f6c4581570edd71b
Shaun <shjsawyer@gmail.com>
2018-08-10 10:10:53 -0400

    roomba class

---
+++ backtracking/roomba.py
@@ -0,0 +1,111 @@
+"""
+This module provides a roomba class, which will enter into a room and clean it.
+An example is provided in the main function.
+
+"""
+
+
+class Roomba:
+    """A roomba vacuum to clean a room.
+
+    <more thorough decription of the class>
+
+    Attributes:
+        room (list of list of characters): room to be cleaned
+        starting_coordinate (2-tuple of ints): coordinate in the room
+            `room` at which to begin and end cleaning.
+
+    """
+    class Heading:
+        ''' Enumeration for the orientation of the Rooma '''
+        NORTH = 0
+        EAST  = 1
+        SOUTH = 2
+        WEST  = 3
+
+    def __init__(self, room, starting_coordinate, heading=Heading.NORTH):
+        ''' Place the roomba in the room so we can start cleaning.
+
+        Args:
+            room (list of list of characters): room to be cleaned
+            starting_coordinate (2-tuple of ints): coordinate in the room
+                `room` at which to begin and end cleaning.
+            heading (Heading): orientation of the roomba
+
+        Returns:
+            None
+
+        '''
+        self.room = room
+        self.current = starting_coordinate
+        self.heading = heading
+
+    def _valid_tile(self, coord):
+        ''' Check if we can move to the tile at `coord` '''
+        try:
+            return self.room[coord[0]][coord[1]] != '#'
+        except IndexError:
+            return False
+
+    def _move(self):
+        ''' Move the roomba one square forward
+
+        Returns:
+            bool: True if roomba has moved, False if it was impossible for
+            roomba to move forward
+
+        Effects:
+            Mutates self.current
+
+        '''
+
+        dx, dy = 0, 0
+        if self.heading == Heading.NORTH or Heading.SOUTH:
+            # We're moving vertically
+            if self.heading == Heading.NORTH:
+                dy += 1
+            else:
+                dy -= 1
+        else:
+            # We're moving horizontally
+            if self.heading == Heading.EAST:
+                dx += 1
+            else:
+                dx -= 1
+        new_coordinate = (self.current[0] + dx, self.current[1] + dy)
+        if self.valid_tile(new_coordinate):
+            self.current = new_coordinate
+            return True
+        else:
+            return False
+
+    def _rotate_cw(self, n):
+        ''' Perform `n` 90 degree rotations clockwise '''
+        self.heading = (self.heading + n) % 4
+
+    def _rotate_ccw(self, n):
+        ''' Perform `n` 90 degree rotations clockwise '''
+        self.heading = (self.heading - n) % 4
+
+    def clean(self):
+        ''' Clean the current spot in the room '''
+        self.room[self.current[0]][self.current[1]] = 'c'
+
+    def clean_room(self):
+        ''' Clean the entire room
+
+        Moves the roomba around the room in a DFS manner, cleaning dirty
+        tiles as we come to them. Returns the roomba to its starting
+        coordinate when it is finished.
+
+        '''
+        # Every time we go to a new tile, we will clean it and add it's valid
+        # neighbours onto a stack to be visited.
+        cleaned, to_clean = [], []
+
+
+
+
+
+
+
